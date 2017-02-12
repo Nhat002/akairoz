e7aa349ddf45f072b2482fdf28f1970e8964c5ea
Tran Vu Xuan Nhat <nhat002@e.ntu.edu.sg>
2017-02-13 00:30:05 +0800

    Change Record API call

--- app/adapters/record.js
+++ app/adapters/record.js
@@ -3,4 +3,8 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
   host: 'http://localhost:8080',
   namespace: 'v1',
+  buildUrl (modelName, id, snapshot, requestType, query) {
+    let userId = query.userId;
+    return `${this.host}/api/${this.namespace}/records?userId=${userId}`;
+  }
 });
--- app/models/record.js
+++ app/models/record.js
@@ -1,8 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr(),
-  recordId: DS.attr(),
+  title: DS.attr(),
   description: DS.attr(),
   status: DS.attr(),
   payment_status: DS.attr(),
